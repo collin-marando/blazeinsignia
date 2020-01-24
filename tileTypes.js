74bdee445820337b56ef9a1de774f2d6dfe699c1
Collin Marando <snippledizzle@users.noreply.github.com>
2020-01-24 08:57:17 -0500

    Rudimentary tile types

--- tileTypes.js
+++ tileTypes.js
@@ -1,20 +1,24 @@
 var tileTypes = {
-	"grass": {
+	"1": {
+		name: "grass",
 		color: color(132, 199, 74),
 		cost: 1,
 		type: floor
 	},
-	"sand": {
+	"2": {
+		name: "sand",
 		color: color(212, 189, 99),
 		cost: 2,
 		type: floor
 	},
-	"lake": {
+	"3": {
+		name: "lake",
 		color: color(44, 151, 209),
 		cost: 1,
 		type: pit
 	}
-	"stone wall": {
+	"4": {
+		name: "stone wall",
 		color: color(146, 150, 150),
 		cost: 1,
 		type: wall
