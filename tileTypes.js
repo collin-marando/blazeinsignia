c8cadc2d6dd71bf3186d3cf50d8a456f8841720f
Collin Marando <snippledizzle@users.noreply.github.com>
2020-01-24 08:56:58 -0500

    Tile types

---
+++ tileTypes.js
@@ -0,0 +1,22 @@
+var tileTypes = {
+	"grass": {
+		color: color(132, 199, 74),
+		cost: 1,
+		type: floor
+	},
+	"sand": {
+		color: color(212, 189, 99),
+		cost: 2,
+		type: floor
+	},
+	"lake": {
+		color: color(44, 151, 209),
+		cost: 1,
+		type: pit
+	}
+	"stone wall": {
+		color: color(146, 150, 150),
+		cost: 1,
+		type: wall
+	}
+}
