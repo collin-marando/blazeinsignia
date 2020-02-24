0ef58b7b6f7a07627299c9a584aa65c786e28f37
Collin Marando <snippledizzle@users.noreply.github.com>
2020-02-23 23:22:53 -0500

    Tile bar displays tile types from tileTypes object

--- mapEditor/mapEditor.js
+++ mapEditor/mapEditor.js
@@ -1,24 +1,25 @@
 //Find another name for cursor if you're going to use the p5 cursor function to make a fuckin sweet flame sword cursor hell yeah dude
-var cursor, grid, data;
+var cursor, map, mapData;
 var pause = false; //for halting printouts
 
-var TILE_SIZE = 30;
-var TILE_BAR_SIZE = 50;
-var BASE_X = 0;
-var BASE_Y = 120;
+var MAP_TILE_SIZE = 30;
+var BAR_TILE_SIZE = 50;
+var MAP_BASE_X = 0;
+var MAP_BASE_Y = 70;
+
 var MOVE_TIME = 4;
 var VIEW_BORDER = 0;
 
 function setup() {
-	var canvas = createCanvas(900, 720);
+	var canvas = createCanvas(900, 670);
 	canvas.parent("canvas");
 
-	data = [[]];
-	tileData = Array(2).fill().map(() => Array(15).fill(0));
+	mapData = [[]];
+	barData = createBarAssets();
 	var zz = {x: 0, y: 0};
 
-	grid = new Grid(zz, 30, 20, MOVE_TIME, drawGrid);
-	tileBar = new Grid(zz, 15, 2, MOVE_TIME, drawTileBar);
+	map = new Grid(zz, 30, 20, MOVE_TIME, drawGrid);
+	tileBar = new Grid(zz, 18, 1, MOVE_TIME, drawTileBar);
 	cursor = new Cursor(zz, MOVE_TIME, drawCursor);
 }
 
@@ -26,15 +27,25 @@ function draw() {
 	if(!pause){
 		background(155);
 		tileBar.draw();
-		grid.draw();
+		map.draw();
 		cursor.draw();
 	}
+}
+
+//--------------BAR FUNCTIONS-------------
+
+function createBarAssets(){
+	var data = [];
+	for(var key in tileTypes){
+		data.push(tileTypes[key]);
+	}
+	return data;
 }
 
 //--------------MAP FUNCTIONS-------------
 
 function validIndex(x, y){
-	return x >= 0 && x < grid.width && y >= 0 && y < grid.height;
+	return x >= 0 && x < map.width && y >= 0 && y < map.height;
 }
 
 //---------------DRAW FUNCTIONS---------------
@@ -42,47 +53,45 @@ function validIndex(x, y){
 function drawCursor(xPos, yPos){
 	fill(0, 255, 0, 128);
 	noStroke();
-	rect(xPos*TILE_SIZE+BASE_X, yPos*TILE_SIZE+BASE_Y, TILE_SIZE, TILE_SIZE, TILE_SIZE/4);
+	rect(xPos*MAP_TILE_SIZE+MAP_BASE_X, yPos*MAP_TILE_SIZE+MAP_BASE_Y, MAP_TILE_SIZE, MAP_TILE_SIZE, MAP_TILE_SIZE/4);
 }
 
 function drawGrid(xPos, yPos){
-	//draw grid tiles
-	for(var i = 0; i < data[0].length; i++){
-		for(var j = 0; j < data.length; j++){
-			data[j][i] ? fill(80, 133, 217) : fill(232, 67, 95);
-			rect((i-xPos)*TILE_SIZE+BASE_X, (j-yPos)*TILE_SIZE+BASE_Y, TILE_SIZE, TILE_SIZE);
+	//draw map tiles
+	for(var i = 0; i < mapData[0].length; i++){
+		for(var j = 0; j < mapData.length; j++){
+			mapData[j][i] ? fill(80, 133, 217) : fill(232, 67, 95);
+			rect((i-xPos)*MAP_TILE_SIZE+MAP_BASE_X, (j-yPos)*MAP_TILE_SIZE+MAP_BASE_Y, MAP_TILE_SIZE, MAP_TILE_SIZE);
 		}
 	}
 	
-	//draw grid lines
+	//draw map lines
 	stroke(200);
 	strokeWeight(1);
-	for(var x = 0; x <= grid.width; x++){
-		line((x-xPos+grid.x)*TILE_SIZE+BASE_X, BASE_Y, (x-xPos+grid.x)*TILE_SIZE+BASE_X, grid.height*TILE_SIZE+BASE_Y);
+	for(var x = 0; x <= map.width; x++){
+		line((x-xPos+map.x)*MAP_TILE_SIZE+MAP_BASE_X, MAP_BASE_Y, (x-xPos+map.x)*MAP_TILE_SIZE+MAP_BASE_X, map.height*MAP_TILE_SIZE+MAP_BASE_Y);
 	}
-	for(var y = 0; y <= grid.height; y++){
-		line(BASE_X, (y-yPos+grid.y)*TILE_SIZE+BASE_Y, grid.width*TILE_SIZE+BASE_X, (y-yPos+grid.y)*TILE_SIZE+BASE_Y);
+	for(var y = 0; y <= map.height; y++){
+		line(MAP_BASE_X, (y-yPos+map.y)*MAP_TILE_SIZE+MAP_BASE_Y, map.width*MAP_TILE_SIZE+MAP_BASE_X, (y-yPos+map.y)*MAP_TILE_SIZE+MAP_BASE_Y);
 	}
 
 }
 
 function drawTileBar(xPos, yPos){
 	//draw tileBar tiles
-	for(var i = 0; i < tileData[0].length; i++){
-		for(var j = 0; j < tileData.length; j++){
-			tileData[j][i] ? fill(80, 133, 217) : fill(232, 67, 95);
-			rect((i-xPos)*TILE_BAR_SIZE, (j-yPos)*TILE_BAR_SIZE, TILE_BAR_SIZE, TILE_BAR_SIZE);
-		}
+	for(var i = 0; i < barData.length; i++){
+		fill(barData[i].color);
+		rect((i-xPos)*BAR_TILE_SIZE, 0, BAR_TILE_SIZE, BAR_TILE_SIZE);
 	}
 	
 	//draw tileBar lines
 	stroke(200);
 	strokeWeight(1);
 	for(var x = 0; x <= tileBar.width; x++){
-		line((x-xPos+tileBar.x)*TILE_BAR_SIZE, 0, (x-xPos+tileBar.x)*TILE_BAR_SIZE, tileBar.height*TILE_BAR_SIZE);
+		line((x-xPos+tileBar.x)*BAR_TILE_SIZE, 0, (x-xPos+tileBar.x)*BAR_TILE_SIZE, tileBar.height*BAR_TILE_SIZE);
 	}
 	for(var y = 0; y <= tileBar.height; y++){
-		line(0, (y-yPos+tileBar.y)*TILE_BAR_SIZE, tileBar.width*TILE_BAR_SIZE, (y-yPos+tileBar.y)*TILE_BAR_SIZE);
+		line(0, (y-yPos+tileBar.y)*BAR_TILE_SIZE, tileBar.width*BAR_TILE_SIZE, (y-yPos+tileBar.y)*BAR_TILE_SIZE);
 	}
 
 }
@@ -91,15 +100,15 @@ function drawTileBar(xPos, yPos){
 
 function moveUp(){
 	if(cursor.y === VIEW_BORDER){
-		grid.moveDown();
+		map.moveDown();
 	} else {
 		cursor.moveUp();
 	}
 }
 
 function moveDown(){
-	if(cursor.y === grid.height-1-VIEW_BORDER){
-		grid.moveUp();
+	if(cursor.y === map.height-1-VIEW_BORDER){
+		map.moveUp();
 	} else {
 		cursor.moveDown();
 	}
@@ -107,15 +116,15 @@ function moveDown(){
 
 function moveLeft(){
 	if(cursor.x === VIEW_BORDER){
-		grid.moveRight()
+		map.moveRight()
 	} else {
 		cursor.moveLeft();
 	}
 }
 
 function moveRight(){
-	if(cursor.x === grid.width-1-VIEW_BORDER){
-		grid.moveLeft()
+	if(cursor.x === map.width-1-VIEW_BORDER){
+		map.moveLeft()
 	} else {
 		cursor.moveRight();
 	}
@@ -125,13 +134,13 @@ function moveRight(){
 //--------------------KEY INPUT---------------------
 
 function keyPressed() {
-	if (keyCode === UP_ARROW && validIndex(cursor.x+grid.x, cursor.y+grid.y-1)){
+	if (keyCode === UP_ARROW && validIndex(cursor.x+map.x, cursor.y+map.y-1)){
 		moveUp();
-	} else if (keyCode === DOWN_ARROW && validIndex(cursor.x+grid.x, cursor.y+grid.y+1)){
+	} else if (keyCode === DOWN_ARROW && validIndex(cursor.x+map.x, cursor.y+map.y+1)){
 		moveDown();
-	} else if (keyCode === LEFT_ARROW && validIndex(cursor.x+grid.x-1, cursor.y+grid.y)){
+	} else if (keyCode === LEFT_ARROW && validIndex(cursor.x+map.x-1, cursor.y+map.y)){
 		moveLeft();
-	} else if (keyCode === RIGHT_ARROW && validIndex(cursor.x+grid.x+1, cursor.y+grid.y)){
+	} else if (keyCode === RIGHT_ARROW && validIndex(cursor.x+map.x+1, cursor.y+map.y)){
 		moveRight();
 	} else if (key === 'p') {
 		pause = !pause;
@@ -143,9 +152,9 @@ function keyPressed() {
 //--------------------MOUSE INPUT---------------------
 
 function canvasCoordsToWindowIndex(x, y){
-	if(x >= BASE_X && x < BASE_X+grid.width*TILE_SIZE && y >= BASE_Y && y < BASE_Y+grid.width*TILE_SIZE){
-		x = int((x-BASE_X)/TILE_SIZE);
-		y = int((y-BASE_Y)/TILE_SIZE);
+	if(x >= MAP_BASE_X && x < MAP_BASE_X+map.width*MAP_TILE_SIZE && y >= MAP_BASE_Y && y < MAP_BASE_Y+map.width*MAP_TILE_SIZE){
+		x = int((x-MAP_BASE_X)/MAP_TILE_SIZE);
+		y = int((y-MAP_BASE_Y)/MAP_TILE_SIZE);
 		return {x: x, y: y};
 	}
 	return undefined;
@@ -155,8 +164,8 @@ function canvasCoordsToWindowIndex(x, y){
 function canvasCoordsToMapIndex(x, y){
 	var index = canvasCoordsToWindowIndex(x, y);
 	if(typeof index === "undefined"){return undefined;}
-	index.x += grid.x;
-	index.y += grid.y;
+	index.x += map.x;
+	index.y += map.y;
 	return index;
 }
 
--- tileTypes.js
+++ tileTypes.js
@@ -1,26 +1,26 @@
 var tileTypes = {
 	"1": {
 		name: "grass",
-		color: color(132, 199, 74),
+		color: 'rgb(132,199,74)',
 		cost: 1,
-		type: floor
+		type: "floor"
 	},
 	"2": {
 		name: "sand",
-		color: color(212, 189, 99),
+		color: "rgb(212, 189, 99)",
 		cost: 2,
-		type: floor
+		type: "floor"
 	},
 	"3": {
 		name: "lake",
-		color: color(44, 151, 209),
+		color: "rgb(44, 151, 209)",
 		cost: 1,
-		type: pit
-	}
+		type: "pit"
+	},
 	"4": {
 		name: "stone wall",
-		color: color(146, 150, 150),
+		color: "rgb(146, 150, 150)",
 		cost: 1,
-		type: wall
+		type: "wall"
 	}
 }
