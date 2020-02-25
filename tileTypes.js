f79170c117cf5a2323f4ac82aa58ba25c59abeae
Collin Marando <snippledizzle@users.noreply.github.com>
2020-02-24 19:29:03 -0500

    Change tile id to be name of tile

--- tileTypes.js
+++ tileTypes.js
@@ -1,24 +1,20 @@
 var tileTypes = {
-	"1": {
-		name: "grass",
+	"grass": {
 		color: 'rgb(132,199,74)',
 		cost: 1,
 		type: "floor"
 	},
-	"2": {
-		name: "sand",
+	"sand": {
 		color: "rgb(212, 189, 99)",
 		cost: 2,
 		type: "floor"
 	},
-	"3": {
-		name: "lake",
+	"lake": {
 		color: "rgb(44, 151, 209)",
 		cost: 1,
 		type: "pit"
 	},
-	"4": {
-		name: "stone wall",
+	"stone_wall": {
 		color: "rgb(146, 150, 150)",
 		cost: 1,
 		type: "wall"
